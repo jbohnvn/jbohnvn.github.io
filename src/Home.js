36688ff8f5c4bf8a1b96ebb33eb148a86665652c
quoctrinh <quoc.trinh@outlook.com>
2019-01-28 10:44:16 +0100

    delete

--- src/Home.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export const Home = () => (
-    <div>
-      <h1>Welcome to the Website!</h1>
-    </div>
-  )
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter} from "react-router-dom";
-
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, rootElement);
